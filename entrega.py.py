ca2e309424b6593c0b582235103d3b736f23b393
miguel09j <110635601+miguel09j@users.noreply.github.com>
2023-03-09 14:58:11 -0500

    Add files via upload

---
+++ entrega.py.py
@@ -0,0 +1,16 @@
+import PARCIAL1RPOGRAMACIONAPLICADAASISTEMASMECATRONICOS
+
+
+def main():
+ print("ingrese un numero entre 2 y 8 digitos")
+
+ opc = int(input("ingrese una opcion: "))
+
+ if opc == 1:
+    pass
+ elif opc == 2:
+    pass
+ 
+
+if __name__== "__main__":
+  main()
